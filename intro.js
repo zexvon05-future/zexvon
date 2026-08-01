1e6e244e1b1cfb36e93ac5c43aeca6128a2cb82f
zexvon05-future <zexvon05@gmail.com>
2026-08-01 18:33:15 +0530

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,3 +1,14 @@
-window.addEventListener("load", () => {
-    console.log("ZEXVON Intro Loaded");
+const hero = document.getElementById("heroImage");
+
+document.addEventListener("mousemove", (e) => {
+  const x = (e.clientX / window.innerWidth - 0.5) * 20;
+  const y = (e.clientY / window.innerHeight - 0.5) * 20;
+
+  hero.style.transform =
+    `perspective(1000px) rotateY(${x}deg) rotateX(${-y}deg) scale(1.03)`;
+});
+
+document.addEventListener("mouseleave", () => {
+  hero.style.transform =
+    "perspective(1000px) rotateY(0deg) rotateX(0deg) scale(1)";
 });
